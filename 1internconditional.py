a32dbaa33ac0c3469c182a45f6cab72cec7041a9
Saumya Dubey <dubeysaumya2726@gmail.com>
2025-08-12 21:14:29 +0530

    Add files via upload

---
+++ 1internconditional.py
@@ -0,0 +1,7 @@
+temp=int(input("enter the temperature"))
+if temp<15:
+    print("Given temperature is cold")
+elif temp in range(15,25):
+    print("Given temperature is warm")
+else :
+    print("Given temperature is hot")        
